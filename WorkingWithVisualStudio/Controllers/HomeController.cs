66c2404360c5860cf84ad8ca1d2daf3483b7d7fb
ali2 <ali62n62@yahoo.com>
2017-07-13 15:55:16 +0430

    BrowserLink not working

--- WorkingWithVisualStudio/Controllers/HomeController.cs
+++ WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WorkingWithVisualStudio.Models;
+using System.Linq;
 
 namespace WorkingWithVisualStudio.Controllers
 {
+    //TODO BrowserLink Didnot Work
     public class HomeController:Controller
     {
         public IActionResult Index()
         {
 
-            return View(SimpleRepository.SharedRepository.Products);
+            return View(SimpleRepository.SharedRepository.Products.
+                Where(p => p?.Price < 50));
         }
             
 
