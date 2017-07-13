5c3eb87e1138ec10db14fe0850599349083ee504
Ali <ali62n62@yahoo.com>
2017-07-13 21:54:05 +0430

    nunit support added

--- Test/WorkingWithVisualStudio.Tests/Test1.cs
+++ Test/WorkingWithVisualStudio.Tests/Test1.cs
@@ -6,13 +6,13 @@ namespace WorkingWithVisualStudio.Tests
     [TestFixture]
     public class Test1
     {
+        private WorkingWithVisualStudio.Models.Product product;
 
         [Test]
         public void t1()
         {
             int a = 1;
-            Assert.That((a == 1), Is.True);
+            Assert.That(a==1,Is.True);
         }
-
     }
 }
