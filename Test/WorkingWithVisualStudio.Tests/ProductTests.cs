8eef185169ef6031597311eab0a1131837489b47
Ali <ali62n62@yahoo.com>
2017-07-13 22:03:04 +0430

    ProductTests class being written

---
+++ Test/WorkingWithVisualStudio.Tests/ProductTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using WorkingWithVisualStudio.Models;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void CanChangeProductName()
+        {
+            // Arrange
+            var p = new Product {Name = "Test", Price = 100M};
+            // Act
+            p.Name = "New Name";
+            //Assert
+            Assert.That(p.Name, Is.EqualTo("New Name"));
+        }
+
+        [Test]
+        public void CanChangeProductPrice()
+        {
+            // Arrange
+            var p = new Product {Name = "Test", Price = 100M};
+            // Act
+            p.Price = 200M;
+            //Assert
+            Assert.That( p.Price,Is.EqualTo(100M));
+        }
+    }
+}
